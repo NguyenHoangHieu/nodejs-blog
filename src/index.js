9b800113e2b8669e0e406d330e6cc1b175ca7662
NguyenHoangHieu <nguyenhoanghieu191@gmail.com>
2021-01-18 22:59:58 +0700

    Commit2: build project

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
+//use static file
+app.use(express.static(path.join(__dirname, 'public')));
+//ex: http://localhost:3000/img/a.jpg
+
 //loger
 app.use(morgan('combined'))
 // app.use(morgan('dev', 'tiny'))
@@ -18,7 +22,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 
-app.get('/abc', (req, res) => {
+app.get('/', (req, res) => {
     return res.render('home');
 })
 
